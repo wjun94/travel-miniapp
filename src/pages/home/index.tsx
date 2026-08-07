4c2403c95a866ed23b75f186fee561eeb751e7f7
wjun94 <994037715@qq.com>
2026-08-07 10:03:01 +0800

    feat: 添加图标

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -172,8 +172,8 @@ export default function HomePage() {
         <View className='flex flex-row items-center ml-4'>
           <Image src={LogoPng} className='w-70px h-70px mr-1.5 rounded-20px' />
           <View className='flex flex-col leading-none'>
-            <Text className='font-bold text-[#e97442] tracking-wide text-30px'>觅途游</Text>
-            <Text className='text-[20px] text-black tracking-widest mt-1'>Meetogo</Text>
+            <Text className='font-bold text-[#e97442] tracking-wide text-30px'>邻刻走</Text>
+            <Text className='text-[24px] text-black tracking-widest mt-1'>LinkGo</Text>
           </View>
         </View>
       </NavBar>
